dd3d624ac9eb016c4b3e4d4c04fcd039e7ad2704
Yogesh Bhalerao <yogeshbhalerao483@gmail.com>
2025-01-29 16:14:06 +0530

    add main method message

--- src/main/java/com/task/management/TaskManagementApplication.java
+++ src/main/java/com/task/management/TaskManagementApplication.java
@@ -9,7 +9,7 @@ public class TaskManagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TaskManagementApplication.class, args);
 		
-		
+		System.out.println("Task Management App is running");
 
 	}
 
